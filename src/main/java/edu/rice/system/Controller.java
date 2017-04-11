e5d59b233e0739b9452180eab987541f110f617c
kiat <kiat@rice.edu>
2017-04-11 17:26:05 -0500

    added some new features in controller and added the Constant class to keep constant values there.

--- src/main/java/edu/rice/system/Controller.java
+++ src/main/java/edu/rice/system/Controller.java
@@ -1,17 +1,44 @@
 package edu.rice.system;
 
+import com.github.andrewoma.dexx.collection.HashMap;
+
 import edu.rice.metadata.MetadataManager;
 
 public class Controller {
 
-	public static void main(String[] args) {
-
-		
+	// This HashMap keeps track of all seen Machines and Dimensions so far. 
+	HashMap<Integer, boolean[]>  machineDimensions;
+	
+	
+	
+	private Controller() {
 		// Reads the metadata and have it ready for use.
 		MetadataManager.getInstance().readMetaData("./src/main/resources/molding_machine_10M.metadata.nt");
-		 
+
+	}
+
+	static class SingletonHolder {
+		static final Controller instance = new Controller();
+	}
+
+	public static Controller getInstance() {
+		return SingletonHolder.instance;
+	}
+
+
+	public void pushData(){
 		
 		
+		
+	}
+	
+	
+	
+	
+	
+	// just for testing.
+	public static void main(String[] args) {
+
 	}
 
 }
---
+++ src/main/java/edu/rice/utils/Constants.java
@@ -0,0 +1,9 @@
+package edu.rice.utils;
+
+public class Constants {
+
+	public static final int Window_Size=10;
+	
+	
+	
+}
